5374480e914f5a32dba6f7a569b39f209b7623f5
Tran <tranle97@gmail.com>
2021-05-22 23:19:33 -0500

    Add devicons for skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -5,12 +5,22 @@ function Skills() {
     <div id='skills'>
       <h3>Skills</h3>
       <div id='skills-container'>
-        <p>React</p>
+        <i class="devicon-javascript-plain colored"></i>
+        <i class="devicon-python-plain"></i>
+        <i class="devicon-nodejs-plain-wordmark colored"></i>
+        <i class="devicon-react-original-wordmark colored"></i>
+        <i class="devicon-redux-original colored"></i>
+        <i class="devicon-express-original"></i>
+        <i className="devicon-heroku-plain-wordmark colored"></i>
+        <i class="devicon-postgresql-plain"></i>
+        <i class="devicon-sequelize-plain colored"></i>
+        <i class="devicon-flask-original-wordmark"></i>
+        {/* <p>React</p>
         <p>Redux</p>
         <p>Python</p>
         <p>Node</p>
         <p>JavaScript</p>
-        <p>Express</p>
+        <p>Express</p> */}
       </div>
     </div>
   )
