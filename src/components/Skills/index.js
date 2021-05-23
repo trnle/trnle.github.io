11ea73489d9cb27a8d9da006def152592c7f2664
Tran <tranle97@gmail.com>
2021-05-23 00:12:30 -0500

    Skills classnames

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -10,8 +10,8 @@ function Skills() {
         <i className="devicon-nodejs-plain-wordmark colored"></i>
         <i className="devicon-react-original-wordmark colored"></i>
         <i className="devicon-redux-original colored"></i>
-        <i class="devicon-html5-plain-wordmark colored"></i>
-        <i class="devicon-css3-plain-wordmark colored"></i>
+        <i className="devicon-html5-plain-wordmark colored"></i>
+        <i className="devicon-css3-plain-wordmark colored"></i>
         <i className="devicon-postgresql-plain-wordmark colored"></i>
         <i className="devicon-express-original"></i>
         <i className="devicon-heroku-plain-wordmark colored"></i>
