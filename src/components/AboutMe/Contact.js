1139fa4ea0a961ea3b39b1912032efc54c4de210
Tran <tranle97@gmail.com>
2021-08-09 12:04:13 -0500

    Add tooltips to contact icons

--- src/components/AboutMe/Contact.js
+++ src/components/AboutMe/Contact.js
@@ -1,21 +1,47 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
+
 function Contact() {
+  const LightTooltip = withStyles((theme) => ({
+    tooltip: {
+      backgroundColor: theme.palette.common.white,
+      color: 'rgba(0, 0, 0, 0.87)',
+      boxShadow: theme.shadows[1],
+      fontSize: 11,
+    },
+    arrow: {
+      color: theme.palette.common.white,
+    }
+  }))(Tooltip);
+
   return (
     <>
       <div id='contact-links'>
         <a href='./Le_Tran_Resume.pdf' title='Resume' target='_blank' rel='noreferrer'>
-          <i className="far fa-file"></i>
+          <LightTooltip title="Resume" arrow>
+            <i className="far fa-file"></i>
+          </LightTooltip>
         </a>
         <a href='https://www.linkedin.com/in/trnle' target='_blank' rel='noreferrer'>
-          <i className='fab fa-linkedin'></i>
+          <LightTooltip title="LinkedIn" arrow>
+            <i className='fab fa-linkedin'></i>
+          </LightTooltip>
         </a>
         <a href='https://github.com/trnle' target='_blank' rel='noreferrer'>
-          <i className='fab fa-github'></i>
+          <LightTooltip title="GitHub" arrow>
+            <i className='fab fa-github'></i>
+          </LightTooltip>
         </a>
         <a href='https://angel.co/u/tran-le-5' target='_blank' rel='noreferrer'>
-          <i className='fab fa-angellist'></i>
+          <LightTooltip title="AngelList" arrow>
+            <i className='fab fa-angellist'></i>
+          </LightTooltip>
         </a>
         <a href='mailto:tranle97@gmail.com'>
-          <i className='fas fa-envelope'></i>
+          <LightTooltip title="Email" arrow>
+            <i className='fas fa-envelope'></i>
+          </LightTooltip>
         </a>
       </div>
     </>
