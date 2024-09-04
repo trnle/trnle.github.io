bb46ea4dea7c1bd4029e63cd98f9f19ea7e5bd75
Tran <tranle97@gmail.com>
2024-09-03 19:34:03 -0700

    Update resume and bio

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -16,8 +16,7 @@ function AboutMe() {
         <h1>Hi, I'm Tran! &#128105;&#127995;&#8205;&#128187;</h1>
         <div id='bio'>
           <p>
-            Currently, I work at Nordstrom as a Software Engineer 1 in the Inventory
-            Deployment and Planning space! I graduated from The University of Texas 
+            Currently, I work at Nordstrom as a Software Engineer 2 in the Merchandise Planning space! I graduated from The University of Texas 
             at Austin (Hook 'em &#129304;&#127996;) with a Bachelor of Science and 
             Arts in Biology. During my time at university, I realized that software 
             engineering was a growing interest that I wanted to pursue as a career. 
@@ -29,7 +28,7 @@ function AboutMe() {
           <p>
             I'm looking forward to growing as a software engineer by continuing
             to familiarize myself with useful programming languages and tools, and
-            I'm especiallykeen on learning about mobile app development in the future!
+            I'm especially keen on learning about mobile app development, design, and management!
           </p>
         </div>
       </div>
